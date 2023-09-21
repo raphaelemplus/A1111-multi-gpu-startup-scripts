4f71c57c426756227c1acfcdda477fc3169b1296
raphaelemplus <140806634+raphaelemplus@users.noreply.github.com>
2023-09-21 14:52:42 +0200

    convenient startup script

---
+++ start_4GPUs.ps1
@@ -0,0 +1,7 @@
+Start-Process -FilePath '.\webui0.bat'
+Start-Sleep -Seconds 1.5
+Start-Process -FilePath '.\webui1.bat'
+Start-Sleep -Seconds 1.5
+Start-Process -FilePath '.\webui2.bat'
+Start-Sleep -Seconds 1.5
+Start-Process -FilePath '.\webui3.bat'
